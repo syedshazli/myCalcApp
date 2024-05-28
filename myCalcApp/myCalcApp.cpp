608294012c170421d7068e8c496a5b791c993c60
syedshazli <syed.abdu1818@gmail.com>
2024-05-28 06:58:13 -0400

    comment

--- myCalcApp/myCalcApp.cpp
+++ myCalcApp/myCalcApp.cpp
@@ -7,6 +7,7 @@ int main()
 {
     std::cout << "Hello World!\n";
     std::cout << "This is a calculator";
+    //lets build something that can change the world!
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
