4b5a3637dcea1f03f53b3e38d6788d5515811aee
syedshazli <syed.abdu1818@gmail.com>
2024-05-30 06:03:19 -0400

    Day three goals updated

--- myCalcApp/myCalcApp.cpp
+++ myCalcApp/myCalcApp.cpp
@@ -2,16 +2,16 @@
 // Main is our driver class that asks the user about initial operation input
 
 #include <iostream>
+#include "operation.h"
 
 int main()
 {
     
     /*
-   
     * 
     *   I think that's all. Push to main 
     * 
-    *  Day two Goals:
+    *  Day Three Goals:
     *   Learn more about OOP in C++
     *   Implement simple calculator interface
     *       Do this using class inheritance and things of that nature
@@ -37,8 +37,16 @@ int main()
     } //end of while, obtained valid user input 
     
 
+    //operation(input); 
+    // will be how we call the function Im assuming
+        //also, whats the difference in project--> add new class and project --> add new item?
 
 } //end of main, move on to class structure
+
+
+
+
+
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
