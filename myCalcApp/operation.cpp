0a0b427bcbd91d23f0448150a2ec9f3fe9a9da9e
syedshazli <syed.abdu1818@gmail.com>
2024-06-05 12:55:33 -0400

    Finished addition operation

--- myCalcApp/operation.cpp
+++ myCalcApp/operation.cpp
@@ -2,5 +2,10 @@
 #include <iostream>
 
 void operation::show_add() {
-	std::cout << "We're adding";
+	float x = 0;
+	float y = 0;
+	std::cout << "What two numbers would you like to add?";
+	std::cin >> x;
+	std::cin >> y;
+	std::cout << "Your sum is " << x + y;
 }
